7ac7a1390dccce089e67e1474fafb1d58a53656a
Andrii Karachuk <Andrii_Karachuk@na.com>
2018-10-21 21:03:57 +0300

    Date-time task 1

---
+++ PracticeTasks/PracticeTasks_1/Tasks/Task1CurrentDateTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task1CurrentDateTime
+    {
+        static DateTime localTime = DateTime.Now;
+        DateTime utcTime = localTime.ToUniversalTime();
+
+        public void PrintTime()
+        {
+            Console.WriteLine("Local time:");
+            Console.WriteLine("{0:MM'/'dd'/'yyyy}", localTime, utcTime);
+            Console.WriteLine(localTime.DayOfWeek + ", {0:dd MMM yyyy}", localTime);
+            Console.WriteLine(localTime.DayOfWeek + ", {0:dd MMM yyyy hh:mm:ss}", localTime);
+
+            Console.WriteLine("UTC time:");
+            Console.WriteLine("{0:MM'/'dd'/'yyyy}", utcTime);
+            Console.WriteLine(localTime.DayOfWeek + ", {0:dd MMM yyyy}", utcTime);
+            Console.WriteLine(localTime.DayOfWeek + ", {0:dd MMM yyyy hh:mm:ss}", utcTime);
+        }
+
+
+    }
+}
