96e103be0fd3dd0a02419bebd640514a7e300f7e
Andrii Karachuk <Andrii_Karachuk@na.com>
2018-10-21 23:51:24 +0300

    Task 6 - fill an array and print it like NxN matrix

--- PracticeTasks/PracticeTasks_1/Tasks/Program.cs
+++ PracticeTasks/PracticeTasks_1/Tasks/Program.cs
@@ -20,8 +20,12 @@ namespace Tasks
             //Task3_Compare2Dates task3 = new Task3_Compare2Dates();
             //task3.compareDates();
 
-            Task4_Difference task4 = new Task4_Difference();
-            task4.printDifference();
+            //Task4_Difference task4 = new Task4_Difference();
+            //task4.printDifference();
+
+            Task6_Matrix task6 = new Task6_Matrix();
+            task6.fillArray();
+            task6.printArray();
 
             Console.ReadKey();
         }
---
+++ PracticeTasks/PracticeTasks_1/Tasks/Task6_Matrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Task6_Matrix
+    {
+        int[,] array = new int[4, 4];
+        Random random = new Random();
+
+        public void fillArray()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    var number = random.Next(1, 10);
+                    array[i, j] = number;
+                }
+            }
+        }
+
+        public void printArray()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+                               
+    }
+}
