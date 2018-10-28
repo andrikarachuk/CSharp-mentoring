6105f033b04318b5af4f7bc23f0649e84bf46903
Andrii Karachuk <Andrii_Karachuk@na.com>
2018-10-28 22:14:51 +0200

    Task 5 - Calculate rectangle Area, Perimether and Diagonal Length with 4 different methods setup

--- PracticeTasks/PracticeTasks_1/Tasks/Program.cs
+++ PracticeTasks/PracticeTasks_1/Tasks/Program.cs
@@ -8,7 +8,6 @@ namespace Tasks
 {
     class Program
     {
-        //Practice project  - do not review
         static void Main(string[] args)
         {
             //Task1_DateTime task1 = new Task1_DateTime();
@@ -23,9 +22,19 @@ namespace Tasks
             //Task4_Difference task4 = new Task4_Difference();
             //task4.printDifference();
 
-            Task6_Matrix task6 = new Task6_Matrix();
-            task6.fillArray();
-            task6.printArray();
+            //Task6_Matrix task6 = new Task6_Matrix();
+            //task6.fillArray();
+            //task6.printArray();
+
+            //Task5_Rectangle task5 = new Task5_Rectangle();
+            //task5.CalculateUsual(); // <- Start here to use usual usual method 
+            ////task5.CalculateRef(task5.sideA, task5.sideB, ref task5.area, ref task5.perimether, ref task5.diagonalLength);
+            ////task5.CalculateOut(task5.sideA, task5.sideB, out task5.area, out task5.perimether, out task5.diagonalLength);
+            ////task5.CalculateParams(out task5.area, out task5.perimether, out task5.diagonalLength, task5.sideA, task5.sideB);
+            
+            //task5.PrintResults();
+
+
 
             Console.ReadKey();
         }
---
+++ PracticeTasks/PracticeTasks_1/Tasks/Task5_Rectangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task5_Rectangle
+    {
+        public double sideA;
+        public double sideB;
+
+        public Task5_Rectangle()
+        {
+            sideA = 5;
+            sideB = 10;
+        }
+
+        public Task5_Rectangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+
+        public double area = 0;
+        public double perimether;
+        public double diagonalLength;
+
+        public void CalculateUsual()
+        {
+            area = sideA * sideB;
+            perimether = 2 * (sideA + sideB);
+            diagonalLength = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+        }
+
+        //public void CalculateRef(double sideA, double sideB, ref double area, ref double perimether, ref double diagonalLength)
+        //{
+        //    area = sideA * sideB;
+        //    perimether = 2 * (sideA + sideB);
+        //    diagonalLength = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+        //}
+
+        //public void CalculateOut(double sideA, double sideB, out double area, out double perimether, out double diagonalLength)
+        //{
+        //    area = sideA * sideB;
+        //    perimether = 2 * (sideA + sideB);
+        //    diagonalLength = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+        //}
+
+        //public void CalculateParams(out double area, out double perimether, out double diagonalLength, params double[] rectangleParams)
+        //{
+        //    area = rectangleParams[0] * rectangleParams[1];
+        //    perimether = 2 * (rectangleParams[0] + rectangleParams[1]);
+        //    diagonalLength = Math.Sqrt(Math.Pow(rectangleParams[0], 2) + Math.Pow(rectangleParams[1], 2));
+        //}
+        public void PrintResults()
+        {
+            Console.WriteLine($"Rectangle Area is: {area} \n" +
+                              $"Rectangle Perimether: {perimether} \n" +
+                              $"Rectangle diagonal length: {diagonalLength}");
+        }
+        
+    }
+}
